aea8995ae400672a7d09eb94d40bf5f1a3ca59ca
jorge <jresino@gmail.com>
2024-09-17 17:08:36 +0200

    psot method

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ FRONT_END_URL = config('FRONT_END_URL')
 print(FRONT_END_URL)
 
 
-@app.post('/ia/')
+@app.get('/')
+async def welcome():
+    return {"response": "hola welcome!"}
+
+
+@app.post('/iaresponse/')
 async def welcome(userMessage : str):
     chat = Chat(userMessage=userMessage)
     chat.iaResponse = "hola que tal" 
